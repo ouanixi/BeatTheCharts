e7064a8bf8d74e391fc0849a00fce118dbf9fb0f
Ouanis Seddaoui <ouanixi@gmail.com>
2016-05-17 12:10:30 +0100

    initial commit

---
+++ desktop/src/com/ouanixi/beatthecharts/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ouanixi.beatthecharts.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ouanixi.beatthecharts.BeatTheChartsMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BeatTheChartsMain(), config);
+	}
+}
